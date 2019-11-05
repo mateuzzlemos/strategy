8f60bc6d58eace033db796e4fd46447642917c61
mateuzzlemos <56963741+mateuzzlemos@users.noreply.github.com>
2019-11-05 15:39:32 -0300

    Create Orcamento.java

---
+++ Orcamento.java
@@ -0,0 +1,14 @@
+package strategy;
+
+public class Orcamento {
+
+    private Double valor;
+
+    public Orcamento(Double valor){
+        this.valor = valor;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+}
